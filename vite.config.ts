8086d81681e093dc12abdb03f4272d50f5c67046
Abdul Aamir <abdulaamir9496@gmail.com>
2025-05-08 16:04:44 +0530

    Updated ports in frontend

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'https://oas-rest-api-evaluator-backend.onrender.com',
